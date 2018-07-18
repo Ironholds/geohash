3bf0b632b1f619adf159985314e513bdc66e7d79
HarryZhu <7harryprince@gmail.com>
2018-07-18 08:19:44 +0800

    fix res_bbox init bug

--- tests/testthat/test_bbox.R
+++ tests/testthat/test_bbox.R
@@ -1,9 +1,10 @@
 testthat::context("Test geohash bbox")
 
 testthat::test_that("A simple lat/lng pair will correctly decoding", {
-  raw_bbox = list()
   raw_bbox <- geohash::gh_decode(geohash::gh_encode(lat = 39.949958,lng = 116.46343,precision = 8))
     
+  res_bbox = list()
+  
   res_bbox[[1]] <- list(lat = raw_bbox$lat - raw_bbox$lat_error,
                        lng = raw_bbox$lng - raw_bbox$lng_error)
   res_bbox[[2]] <- list(lat = raw_bbox$lat - raw_bbox$lat_error,
